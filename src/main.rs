87651326544e8c934121b1c313d9183919450132
asend50 <asend50@kprschools.ca>
2026-03-27 10:42:22 -0400

    basiccalculator

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ Program Details: <The purpose of this program is to allow the user to type in tw
 mod modules;
 
 use crate::modules::grid::draw_grid;
+use crate::modules::label::Label;
 use crate::modules::text_button::TextButton;
 use crate::modules::text_input::TextInput;
-use crate::modules::label::Label;
 use macroquad::prelude::*;
 
 /// Set up window settings before the app runs
@@ -26,19 +26,19 @@ fn window_conf() -> Conf {
     }
 }
 
-fn add(num1:f64, num2: f64) -> f64 {
+fn add(num1: f64, num2: f64) -> f64 {
     num1 + num2
 }
 
-fn subtract(num1:f64, num2: f64) -> f64 {
+fn subtract(num1: f64, num2: f64) -> f64 {
     num1 - num2
 }
 
-fn multiply(num1:f64, num2: f64) -> f64 {
+fn multiply(num1: f64, num2: f64) -> f64 {
     num1 * num2
 }
 
-fn divide(num1:f64, num2: f64) -> f64 {
+fn divide(num1: f64, num2: f64) -> f64 {
     num1 / num2
 }
 
@@ -50,9 +50,9 @@ async fn main() {
 
     let mut txt_num2 = TextInput::new(550.0, 225.0, 150.0, 40.0, 25.0);
 
-    let mut lbl_answer = Label::new("", 553.0, 328.0, 30);
+    let mut lbl_answer = Label::new("=", 553.0, 328.0, 30);
 
-    let mut lbl_help = Label::new("Type a number in \neach box and click\none of the operators", 220.0, 325.0, 25);
+    let lbl_help = Label::new("Type a number in \neach box and click\none of the operators", 220.0, 325.0, 25);
 
     let btn_add = TextButton::new(450.0, 125.0, 50.0, 50.0, "+", BLUE, PURPLE, 30);
 
@@ -69,43 +69,66 @@ async fn main() {
         if btn_exit.click() {
             break;
         }
-        
 
         if btn_add.click() {
-            let num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
-            let num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
-                lbl_answer.set_text(&format!("{}",  add(num1, num2)));
-                
+            let _num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
+            let _num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
+            let num1 = txt_num1.get_text().parse::<f64>();
+            let num2 = txt_num2.get_text().parse::<f64>();
+            if let (Ok(num1), Ok(num2)) = (num1, num2) {
+               
+                lbl_answer.set_text(&format!("= {}", add(num1, num2)));
+            } else {
+                lbl_answer.set_text("Invalid");
             }
+        }
         
 
         if btn_subtract.click() {
-            let num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
-            let num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
-            lbl_answer.set_text(&format!("{}", subtract(num1, num2)));
+            let _num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
+            let _num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
+            let num1 = txt_num1.get_text().parse::<f64>();
+            let num2 = txt_num2.get_text().parse::<f64>();
+            if let (Ok(num1), Ok(num2)) = (num1, num2) {
+               
+                lbl_answer.set_text(&format!("= {}", subtract(num1, num2)));
+            } else {
+                lbl_answer.set_text("Invalid");
+            }
         }
-        
 
         if btn_multiply.click() {
-            let num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
-            let num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
-            lbl_answer.set_text(&format!("{}", multiply(num1, num2)));
+            let _num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
+            let _num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
+            let num1 = txt_num1.get_text().parse::<f64>();
+            let num2 = txt_num2.get_text().parse::<f64>();
+            if let (Ok(num1), Ok(num2)) = (num1, num2) {
+               
+                lbl_answer.set_text(&format!("= {}", multiply(num1, num2)));
+            } else {
+                lbl_answer.set_text("Invalid");
+            }
         }
+
         
 
-         if btn_divide.click() {
-            let num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
-            let num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
-            lbl_answer.set_text(&format!("{}", divide(num1, num2)));
-         }
-         
-         
+        if btn_divide.click() {
+            let _num1: f64 = txt_num1.get_text().trim().parse().unwrap_or(0.0);
+            let _num2: f64 = txt_num2.get_text().trim().parse().unwrap_or(0.0);
+            let num1 = txt_num1.get_text().parse::<f64>();
+            let num2 = txt_num2.get_text().parse::<f64>();
+            if let (Ok(num1), Ok(num2)) = (num1, num2) {
+               
+                lbl_answer.set_text(&format!("= {}", divide(num1, num2)));
+            } else {
+                lbl_answer.set_text("Invalid");
+            }
+        }
 
         txt_num1.draw();
         txt_num2.draw();
         lbl_answer.with_border(BLACK, 3.0).with_fixed_size(153.0, 53.0).draw();
         lbl_help.draw();
         next_frame().await;
-    }
-    }
+    }}
     
